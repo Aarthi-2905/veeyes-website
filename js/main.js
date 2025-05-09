4a59ed506682d63c44bafaf3028ca45c530b0d2c
Kaviyamurugadass <kaviyamurugadass2@gmail.com>
2025-05-09 17:38:03 +0530

    Added the arrow for the submenu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { Card } from './card.js';
+import { Card } from '../card.js';
 
 document.body.appendChild(Card("Hello", "This is a reusable card."));
 document.body.appendChild(Card("Another Card", "Reusable again!"));
